bd50fc9b6c6d194fa51166f1a44de8bcafcb03f9
Adam Jonas <adamjonas25@gmail.com>
2021-01-13 15:22:27 +0000

    Add centering to print names in centrally  aligned output

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "to finish, just hit return twice"
+  puts 'Please enter the names of the students'
+  puts 'to finish, just hit return twice'
 
   students = []
   name = gets.chomp
@@ -14,20 +14,21 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts 'The students of Villains Academy'.center(45)
+  puts ''.center(45, '-')
 end
 
 def print(students)
   students.each_with_index do |student, i|
     if student[:name].start_with?('A') && student[:name].length < 12
-      puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(45)
     end
   end
 end
 # finally, we print the total
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts ''.center(45, '-')
+  puts "Overall, we have #{students.count} great students".center(45)
 end
 # nothing happens until we call the methods
 students = input_students
