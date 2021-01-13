18210a1cc9303965bfa1db290380fe3f016fdd34
Adam Jonas <adamjonas25@gmail.com>
2021-01-13 18:28:45 +0000

    change output to print students by cohort + improve output visuals

--- directory.rb
+++ directory.rb
@@ -1,28 +1,27 @@
+$wrap = "\n" + "".center(80, "-") + "\n"
+
 def input_students
-  puts ''.center(80, '-')
-  puts "Please enter the students' names into the directory.".center(80)
-  puts 'To finish, just hit return twice.'.center(80)
-  puts ''.center(80, '-')
-  puts 'Enter the name and cohort of the first student, seperated by a space:'
+  puts $wrap + "Please enter the students' names into the directory.".center(80)
+  puts 'To finish, just hit return twice.'.center(80) + $wrap
+  puts 'Enter student name:'
 
   students = []
-  name_cohort = gets.center(80).chomp.split
-  name, cohort = name_cohort[0], name_cohort[1].to_sym
+  name = gets.chomp.capitalize
 
-  while !name.empty? do
+  while !name.empty?
+    puts 'Enter their cohort:'
+    cohort = gets.chomp.to_sym.capitalize
     puts 'Enter their country of residence:'
-    country = gets.chomp
-
+    country = gets.chomp.capitalize
     puts 'Enter their hobbies each seperated by a comma and a space:'
     hobbies = gets.chomp.split(', ')
-
     puts 'Enter their height in cm:'
     height = gets.chomp.to_i
 
     students << { name: name, country: country, hobbies: hobbies, height: height, cohort: cohort }
-    puts "Now we have #{students.count} students."
 
-    puts 'Enter the name and cohort of the next student:'
+    puts "Now we have #{students.count} students." + $wrap
+    puts 'Enter next student name:'
     name = gets.chomp
   end
   students
@@ -30,26 +29,31 @@ end
 
 def print_header
   puts 'The Students of Villains Academy'.center(80)
-  puts ''.center(80, '-')
 end
 
-def print(students)
-  i = 0
-  while i < students.length
-    if students[i][:name].start_with?('A') && students[i][:name].length < 12
-      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(80)
-    end
-    i += 1
+def sort_by_cohort(students)
+  sorted_cohorts = {}
+  students.each do |student|
+    sorted_cohorts.include?(student[:cohort]) ?
+    sorted_cohorts[student[:cohort]] << student[:name] :
+    sorted_cohorts[student[:cohort]] = [student[:name]]
+  end
+  sorted_cohorts
+end
+
+def print_cohorts(sorted_cohorts)
+  sorted_cohorts.each do |cohort, students|
+    puts $wrap + "*** #{cohort.to_s.capitalize} cohort ***".center(80) + $wrap
+    puts students.map.with_index { |student, i| "#{i+1}. #{student}".center(80) }
   end
 end
-# finally, we print the total
+
 def print_footer(students)
-  puts ''.center(80, '-')
-  puts "Overall, we have #{students.count} great students".center(80)
-  puts ''.center(80, '-')
+  puts $wrap + "Overall, we have #{students.count} great students!".center(80) + $wrap
 end
 # nothing happens until we call the methods
 students = input_students
+cohorts = sort_by_cohort(students)
 print_header
-print(students)
+print_cohorts(cohorts)
 print_footer(students)
