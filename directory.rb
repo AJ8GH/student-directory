3e510656f9222b399f2b6ed9bea44ba4a673b5b7
Adam Jonas <adamjonas25@gmail.com>
2021-01-13 16:19:42 +0000

    Replace hardcoding of cohort with user input

--- directory.rb
+++ directory.rb
@@ -1,11 +1,13 @@
 def input_students
-
-  puts 'Please enter the names of the students into the directory.'
-  puts 'To finish, just hit return twice.'
-  puts 'Enter the name of the first student:'
+  puts ''.center(80, '-')
+  puts "Please enter the students' names into the directory.".center(80)
+  puts 'To finish, just hit return twice.'.center(80)
+  puts ''.center(80, '-')
+  puts 'Enter the name and cohort of the first student, seperated by a space:'
 
   students = []
-  name = gets.chomp
+  name_cohort = gets.center(80).chomp.split
+  name, cohort = name_cohort[0], name_cohort[1].to_sym
 
   while !name.empty? do
     puts 'Enter their country of residence:'
@@ -17,33 +19,34 @@ def input_students
     puts 'Enter their height in cm:'
     height = gets.chomp.to_i
 
-    students << { name: name, country: country, hobbies: hobbies, height: height, cohort: :february }
+    students << { name: name, country: country, hobbies: hobbies, height: height, cohort: cohort }
     puts "Now we have #{students.count} students."
 
-    puts 'Enter the name of the next student:'
+    puts 'Enter the name and cohort of the next student:'
     name = gets.chomp
   end
   students
 end
 
 def print_header
-  puts 'The students of Villains Academy'.center(45)
-  puts ''.center(45, '-')
+  puts 'The Students of Villains Academy'.center(80)
+  puts ''.center(80, '-')
 end
 
 def print(students)
   i = 0
   while i < students.length
     if students[i][:name].start_with?('A') && students[i][:name].length < 12
-      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(45)
+      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(80)
     end
     i += 1
   end
 end
 # finally, we print the total
 def print_footer(students)
-  puts ''.center(45, '-')
-  puts "Overall, we have #{students.count} great students".center(45)
+  puts ''.center(80, '-')
+  puts "Overall, we have #{students.count} great students".center(80)
+  puts ''.center(80, '-')
 end
 # nothing happens until we call the methods
 students = input_students
